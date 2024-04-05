23efebec9ae8bf07ce2dc6d857365f5832c37b5a
James Holfert <jimholfert@hotmail.com>
2024-04-05 08:27:37 +0100

    End of section 2

---
+++ API/Controllers/BaseAPIController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //Localhost:5000/weatherforecast
+
+    public class BaseAPIController : ControllerBase
+    {
+        
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,17 @@
+using Domain;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Domain.Activity> Activities { get; set; }
+
+        
+    }
+}
