63d7de6e013c382c3e01beb261fc00cdc63a1672
James Holfert <jimholfert@hotmail.com>
2024-04-29 19:00:04 +0100

    End of section 5

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Container, Header, List, Segment } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-
+import {v4 as uuid} from 'uuid';
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -38,6 +38,17 @@ function App() {
     setEditMode(false);
   }
 
+  function handleCreateOrEditActivity (activity: Activity){
+    activity.id ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
+    : setActivities([...activities, {...activity, id: uuid()}]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+  }
+
+  function handleDeleteActivity (id: string){
+    setActivities([...activities.filter(x => x.id !== id)])
+  }
+
   return (
     <div>
       <NavBar openForm={handleFormOpen} />
@@ -49,6 +60,8 @@ function App() {
         editMode = {editMode}
         openForm = {handleFormOpen}
         closeForm = {handleFormClose}
+        createOrEdit = {handleCreateOrEditActivity}
+        deleteActivity = {handleDeleteActivity}
         />
       </Container>
     </div>
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, List } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import ActivityList from './ActivityList';
 import ActivityDetails from '../details/ActivityDetails';
@@ -14,14 +14,20 @@ interface Props{
     editMode: boolean;
     openForm: (id: string) => void;
     closeForm: () => void;
+    createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
 }
 
 export default function ActivityDashboard({activities, selectedActivity, selectActivity, 
-    editMode, openForm, closeForm, cancelSelectActivity}: Props){
+    editMode, openForm, closeForm, cancelSelectActivity, createOrEdit, deleteActivity}: Props){
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} selectActivity={selectActivity}/>
+                <ActivityList 
+                activities={activities} 
+                selectActivity={selectActivity}
+                deleteActivity={deleteActivity} 
+                />
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
@@ -32,7 +38,7 @@ export default function ActivityDashboard({activities, selectedActivity, selectA
                 />
                 }
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} />}
+                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
             </Grid.Column>
         </Grid>
     )
